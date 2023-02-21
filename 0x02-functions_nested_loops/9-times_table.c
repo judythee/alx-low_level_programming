001010cd3e47eea4e37b5045aff853000cfa6849
Your Name <you@example.com>
2023-02-21 13:58:54 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j + i;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -19,6 +19,7 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
