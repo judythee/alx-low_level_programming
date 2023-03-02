8f8c1d0110cf27792046c372fdd0e2035a798f2b
Your Name <you@example.com>
2023-03-02 10:41:04 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - program prints a[2] = 98, followed by a new line.
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
